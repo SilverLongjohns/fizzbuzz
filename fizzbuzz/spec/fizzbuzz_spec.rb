14a2bb13921c91b2e75c968b9b34f0a360f3c3b6
EC2 Default User <ec2-user@ip-172-31-61-189.ec2.internal>
2020-07-01 10:52:39 +0000

    rspec completed

--- fizzbuzz/spec/fizzbuzz_spec.rb
+++ fizzbuzz/spec/fizzbuzz_spec.rb
@@ -11,3 +11,21 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(5)).to eq 'buzz'
     end
 end
+
+describe 'fizzbuzz' do
+    it 'returns "fizz" when passed a multiple of 3' do
+        expect(fizzbuzz(42)).to eq 'fizz'
+    end
+end
+    
+describe 'fizzbuzz' do
+    it 'returns  "buzz" when passed a multiple of 5' do
+        expect(fizzbuzz(25)).to eq 'buzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" if devisible by 3 and 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+end
